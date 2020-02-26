41b1d97a73fc6d928740c233cb7df81c8095bf66
laurencebrubis <laurencebrubis@gmail.com>
2020-02-26 20:06:09 +0800

    tesy

---
+++ database/factories/LaravelFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Laravel;
+use Faker\Generator as Faker;
+
+$factory->define(Laravel::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
